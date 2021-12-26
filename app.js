6dca6e82b8dd3ada3ee4dcfc1fdeb832be5f2ecf
Mekel Tensley <mekelintech@gmail.com>
2021-12-26 00:58:08 -0500

    Add a static function and new instance that counts car doors

--- app.js
+++ app.js
@@ -10,9 +10,18 @@ class Car {
     carStats() {
         return `This car has ${this.doors} doors, a ${this.engine} engine and a beautiful ${this.color} color!`;
     }
+
+    static totalDoors(car1, car2) {
+        const doors1 = car1.doors;
+        const doors2 = car2.doors;
+
+        return doors1 + doors2;
+    }
 }
 // an instance of class that is defining the parameters of that class!
 const cx5 = new Car(4, 'V6', 'grey');
+const civic = new Car(3, 'v4', 'blue');
 
 console.log(cx5);
 console.log(cx5.carStats());
+console.log(Car.totalDoors(cx5, civic));
